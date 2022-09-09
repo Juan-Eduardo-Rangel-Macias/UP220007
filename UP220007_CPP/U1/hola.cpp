9e4e142ce4e94fb7810e90cabcea28f813865fea
Juan Eduardo Rangel Macias <up220007@almunos.upa.edu.mx>
2022-09-09 13:49:55 -0500

    modificcation
    mesage modification

--- UP220007_CPP/U1/hola.cpp
+++ UP220007_CPP/U1/hola.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    cout << "hola mundo que tal todo todo aqui esta bien" << endl;
+    cout << "hola mundo que tal todo todo aqui esta bien me despido" << endl;
     getchar();
     return 0;
 }
