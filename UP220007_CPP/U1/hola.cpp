804e20762717d159699b29bb634e6be38726aea3
UP220007 <up220007@alumnos.upa.edu.mx>
2022-09-09 19:27:58 -0500

    probando git desktop
    realizando pruebas con git desktop

--- UP220007_CPP/U1/hola.cpp
+++ UP220007_CPP/U1/hola.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    cout << "hola mundo que tal todo todo aqui esta bien me despido" << endl;
+    cout << "hola mundo que tal todo todo aqui esta bien me despido todo genial" << endl;
     getchar();
     return 0;
 }
