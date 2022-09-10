cafe2275761ee654a09427140a5ecc281bf0e5f1
Juan Eduardo Rangel Macias <up220007@almunos.upa.edu.mx>
2022-09-09 20:08:56 -0500

    update code
    make some changes in the main code

--- UP220007_CPP/U1/hola.cpp
+++ UP220007_CPP/U1/hola.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    cout << "hola mundo que tal todo todo aqui esta bien me despido todo genial" << endl;
+    cout << "hola mundo que tal todo todo aqui esta bien me despido todo genial y todo correcto" << endl;
     getchar();
     return 0;
 }
