69b69ee5fc0819490429a016ed0afeceb420a568
Juan Eduardo Rangel Macias <up220007@almunos.upa.edu.mx>
2022-10-04 13:44:46 -0500

    tablas
    do the multplicaton tables

---
+++ U2/E03_tablsas.cpp
@@ -0,0 +1,33 @@
+/*********************************************************************
+Unidad 2 tABLAS
+Juan Eduardo Rangel Macias UP220007 
+4/10/2022                    ISC03A
+Realizar Realizar una tabla de multiplicar con ciclos 
+*********************************************************************/
+
+// libreria para manejo de entradas
+#include <iostream>
+
+// libreria para uso de scanf y print f
+#include <stdio.h>
+//#include<stdlib.h>
+
+// uso del namespace para evitar el std::
+using namespace std;
+
+int main()
+{
+  /* code */
+  int n = 0;
+  int r=0;
+  printf("¿De que numero deseas conocer la talba de multiplicar?");
+  scanf(" %d",&n);
+  for (size_t i = 1; i <= 10; i++)
+  {
+    r = i * n;
+    printf("\n%dX%d=%d",n,i,r);
+  }
+  
+  getchar();
+  return 0;
+}
