7879e5560fedf99a599b5d81d360fe35d90538f2
Juan Eduardo Rangel Macias <up220007@almunos.upa.edu.mx>
2022-10-14 12:25:55 -0500

    COMIT
    COMIT

---
+++ U2/E07_formula_general.cpp
@@ -0,0 +1,42 @@
+/*********************************************************************
+Unidad 
+Juan Eduardo Rangel Macias UP220007 
+ / /2022                     ISC03A
+Realizar 
+*********************************************************************/
+
+// libreria para manejo de entradas
+#include <iostream>
+
+// libreria para uso de scanf y print f
+#include <stdio.h>
+#include <math.h>
+//#include<stdlib.h>
+
+// uso del namespace para evitar el std::
+using namespace std;
+
+int main()
+{
+  int c;
+  char salida;
+  c=0;
+  while (c==0)
+  {
+    
+    
+    printf("\ndesea volver a calcular su bono? s/n"); // sustituir ' por comillas dobles en los printf y scanf y agregar el contrabarra n
+    scanf(" %c",&salida);
+    if (toupper(salida)=='S')
+    {
+      c=0;
+    }
+    if (toupper(salida)=='N')
+    {
+      c=1;
+    }
+  }
+  
+  getchar();
+  return 0;
+}
